2ad38e446b9f8d8f0c785202fffb55df95a59ad8
kaluramkanesh <kaluramkanesh123@gmail.com>
2024-03-22 23:29:04 +0530

    fixe some issues

--- index.js
+++ index.js
@@ -1,56 +1,122 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require('cors')
+// const app = express();
+// const http = require("http");
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+// const PORT = process.env.PORT || 3000;
+// const path = require("path");
+// const { userInfo } = require("os");
+// app.use(cors())
+// io.on("connection", function (socket) {
+//   console.log("A user connected");
+//   socket.userOnline = true;
+//   const onlineStatus = socket.userOnline ? "online" : "offline";
+//   socket.on("currentUser", (data) => {
+//     const userInfo = {
+//       onlineStatus: onlineStatus,
+//       userName: data,
+//     };
+//     io.sockets.emit("newMsg", userInfo);
+//   });
+//   socket.on("message", (msg) => {
+//     const formattedMsg = {
+//       user: msg.user,
+//       message: msg.message,
+//     };
+
+//     socket.broadcast.emit("newUser", formattedMsg);
+//   });
+
+//   //Whenever someone disconnects this piece of code executed
+//   socket.on("disconnect", function () {
+//     socket.userOnline = false;
+//     const onlineStatus = socket.userOnline ? "online" : "offline";
+//     socket.on("currentUser", (data) => {
+//       const userInfo = {
+//         onlineStatus: onlineStatus,
+//         userName: data,
+//       };
+//       io.sockets.emit("newMsg", userInfo);
+//     });
+//     console.log("A user disconnected");
+//   });
+// });
+// app.get("/", (req, res) => {
+//   var options = {
+//     root: path.join(__dirname),
+//   };
+//   res.sendFile("/index.html", options);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`server is running on port ${PORT}`);
+// });
+
+
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require('cors');
 const http = require("http");
-const server = http.createServer(app);
+const path = require("path");
 const { Server } = require("socket.io");
 
-const io = new Server(server);
 const PORT = process.env.PORT || 3000;
-const path = require("path");
-const { userInfo } = require("os");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Middleware
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Socket.io Connection
 io.on("connection", function (socket) {
   console.log("A user connected");
+
+  // Setting initial online status
   socket.userOnline = true;
-  const onlineStatus = socket.userOnline ? "online" : "offline";
+
   socket.on("currentUser", (data) => {
+    const onlineStatus = socket.userOnline ? "online" : "offline";
     const userInfo = {
       onlineStatus: onlineStatus,
       userName: data,
     };
     io.sockets.emit("newMsg", userInfo);
   });
+
   socket.on("message", (msg) => {
     const formattedMsg = {
       user: msg.user,
       message: msg.message,
     };
-
     socket.broadcast.emit("newUser", formattedMsg);
   });
 
-  //Whenever someone disconnects this piece of code executed
+  // Handle user disconnection
   socket.on("disconnect", function () {
     socket.userOnline = false;
     const onlineStatus = socket.userOnline ? "online" : "offline";
-    socket.on("currentUser", (data) => {
-      const userInfo = {
-        onlineStatus: onlineStatus,
-        userName: data,
-      };
-      io.sockets.emit("newMsg", userInfo);
-    });
+    const data = ''; // Add your logic to get the username if needed
+    const userInfo = {
+      onlineStatus: onlineStatus,
+      userName: data,
+    };
+    io.sockets.emit("newMsg", userInfo);
     console.log("A user disconnected");
   });
 });
+
+// Routes
 app.get("/", (req, res) => {
-  var options = {
-    root: path.join(__dirname),
-  };
-  res.sendFile("/index.html", options);
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
+// Start server
 server.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
